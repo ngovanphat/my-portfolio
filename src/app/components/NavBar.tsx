4a97226fa24fc855022d02dc1c1ce64fab61b732
phat-nv <1753082@student.hcmus.edu.vn>
2023-06-17 18:46:39 +0700

    modify icon, add sidebar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -12,22 +12,22 @@ export function NavBar(props: NavBarProps) {
   return (
     <nav
       className={classNames({
-        "w-full h-16 py-4 px-2 md:px-8 ": true,
+        "w-full h-16 py-4 px-2 sm:px-8": true,
         "text-black bg-white": true,
         "flex justify-between items-center": true,
         relative: true,
       })}
     >
-      <div className="hidden md:flex">
+      <div className="hidden sm:flex">
         <Link href="/">
           <p className="text-slate-600 text-sm">About me</p>
         </Link>
         <Link href="/">
-          <p className="text-slate-600 text-sm ml-3">Resume</p>
+          <p className="text-slate-600 text-sm ml-8 ">Resume</p>
         </Link>
       </div>
 
-      <div className="md:hidden" onClick={props.handleClickOpenSideBar}>
+      <div className="sm:hidden" onClick={props.handleClickOpenSideBar}>
         <Bars3Icon className="w-8 h-8" />
       </div>
 
@@ -35,16 +35,17 @@ export function NavBar(props: NavBarProps) {
         src={logoImg}
         alt="logo"
         className={classNames({
-          "rounded-full w-10 h-10": true,
+          "rounded-full w-12 h-12": true,
           "select-none": true,
         })}
       />
       <button
         className={classNames({
-          "py-2 px-2 md:px-4 rounded-full": true,
+          "py-2 px-2 sm:px-4 rounded-full": true,
           "bg-gray-200": true,
-          "text-black text-xs md:text-sm font-medium": true,
+          "text-black text-xs sm:text-sm font-medium": true,
           "select-none": true,
+          "hidden sm:block": true,
         })}
       >
         Contact me
--- src/app/components/layouts/DashboardLayout.tsx
+++ src/app/components/layouts/DashboardLayout.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { NavBar } from "../NavBar";
+import Sidebar from "../SideBar";
 
 export default function DashBoardLayout({
   children,
@@ -9,10 +10,12 @@ export default function DashBoardLayout({
   children: React.ReactNode;
 }) {
   const [openSideBar, setOpenSideBar] = useState(false);
-  console.log(openSideBar);
   return (
     <main className="w-full h-full bg-white">
       <NavBar handleClickOpenSideBar={() => setOpenSideBar(!openSideBar)} />
+      <div className="sm:hidden block">
+        <Sidebar open={openSideBar} setOpen={setOpenSideBar} />
+      </div>
       {children}
     </main>
   );
